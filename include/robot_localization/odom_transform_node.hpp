089a1c92a390a423fe23c662b5daa7c8ff8e1328
redvinaa <redvinaa@gmail.com>
2021-12-17 15:52:44 +0100

    Write odom_transform_node
    Untested, but compiles

---
+++ include/robot_localization/odom_transform_node.hpp
@@ -0,0 +1,31 @@
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "geometry_msgs/msg/pose_with_covariance_stamped.hpp"
+#include "rclcpp/publisher_base.hpp"
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp/subscription.hpp"
+#include "nav_msgs/msg/odometry.hpp"
+#include "tf2_ros/transform_listener.h"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "tf2_geometry_msgs/tf2_geometry_msgs.h"
+#include "tf2_ros/buffer.h"
+#include "tf2/convert.h"
+#include <memory>
+#include <string>
+
+class OdomTransform : public rclcpp::Node
+{
+private:
+    std::string odom_topic, pose_topic, original_frame, new_frame;
+    std::unique_ptr<tf2_ros::Buffer> buffer;
+    std::shared_ptr<tf2_ros::TransformListener> transform_listener;
+
+    rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub;
+    rclcpp::Publisher<geometry_msgs::msg::PoseWithCovarianceStamped>::SharedPtr pose_pub;
+    geometry_msgs::msg::TransformStamped transform;
+
+    void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) const;
+
+public:
+    OdomTransform();
+};
---
+++ src/odom_transform_node.cpp
@@ -0,0 +1,66 @@
+#include "robot_localization/odom_transform_node.hpp"
+
+
+OdomTransform::OdomTransform() : Node("odom_transform")
+{
+    // get parameters
+    this->declare_parameter<std::string>("odom_topic");
+    this->declare_parameter<std::string>("pose_topic");
+    this->declare_parameter<std::string>("original_frame");
+    this->declare_parameter<std::string>("new_frame");
+
+    odom_topic     = this->get_parameter("odom_topic").as_string();
+    pose_topic     = this->get_parameter("pose_topic").as_string();
+    original_frame = this->get_parameter("original_frame").as_string();
+    new_frame      = this->get_parameter("new_frame").as_string();
+
+    // initialize tf2
+    buffer = std::make_unique<tf2_ros::Buffer>(this->get_clock());
+    transform_listener = std::make_shared<tf2_ros::TransformListener>(*buffer);
+    transform = buffer->lookupTransform(new_frame, original_frame, rclcpp::Time(0));
+
+    // initialize odom subscriber
+    odom_sub = this->create_subscription<nav_msgs::msg::Odometry>(odom_topic, 10,
+        std::bind(&OdomTransform::odom_callback, this, std::placeholders::_1));
+
+    // initialize pose publisher
+    pose_pub = this->create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>(pose_topic, 10);
+}
+
+
+void OdomTransform::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg) const
+{
+    // convert Odometry to Pose
+    geometry_msgs::msg::Pose pose_before, pose;
+    pose_before.position.x = msg->pose.pose.position.x;
+    pose_before.position.y = msg->pose.pose.position.y;
+    pose_before.position.z = msg->pose.pose.position.z;
+    pose_before.orientation.x = msg->pose.pose.orientation.x;
+    pose_before.orientation.y = msg->pose.pose.orientation.y;
+    pose_before.orientation.z = msg->pose.pose.orientation.z;
+    pose_before.orientation.w = msg->pose.pose.orientation.w;
+
+    // geometry_msgs::msg::Point pt1, pt2;
+    // tf2::doTransform(pt1, pt2, transform);
+
+    // create PoseWithCovarianceStamped
+    geometry_msgs::msg::PoseWithCovariance posec;
+    posec.pose = pose;
+    posec.covariance = msg->pose.covariance;
+    geometry_msgs::msg::PoseWithCovarianceStamped posecs;
+    posecs.header = msg->header;
+    posecs.pose = posec;
+
+    // publish pose
+    pose_pub->publish(posecs);
+}
+
+
+int main(int argc, char** argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<OdomTransform>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
