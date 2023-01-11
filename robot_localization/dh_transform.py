848a00d10124a8f0f405d83dbf17a4c9f962d38e
Vince Reda <60265874+redvinaa@users.noreply.github.com>
2023-01-11 12:28:07 +0100

    Fix python module errors (#7)

---
+++ robot_localization/dh_transform.py
@@ -0,0 +1,66 @@
+#! /usr/bin/env python3
+
+import numpy as np
+from geometry_msgs.msg import Transform, Pose, Vector3, Quaternion
+from scipy.spatial.transform import Rotation
+from robot_localization.conversion import convert
+
+
+def to_homogenius(mat: np.ndarray) -> np.ndarray:
+    if mat.ndim == 1:  # vector
+        new = np.ones((mat.shape[0] + 1,), dtype=mat.dtype)
+        new[0:-1] = mat
+    elif mat.ndim == 2:  # matrix
+        new = np.zeros((mat.shape[0] + 1, mat.shape[1] + 1), dtype=mat.dtype)
+        new[:-1, :-1] = mat
+        new[-1, -1] = 1
+    else:  # ???
+        raise NotImplementedError
+    return new
+
+
+def Transform_to_T(tf: Transform, *, invert: bool = False) -> np.ndarray:
+    T_trans = convert(tf.translation, np.ndarray)
+    if invert:
+        T_trans = -T_trans
+
+    q = convert(tf.rotation, np.ndarray)
+    T_rot = Rotation.from_quat(q).as_matrix()
+    if invert:
+        T_rot = np.linalg.inv(T_rot)
+
+    T = to_homogenius(T_rot)
+    T[:3, -1] = T_trans
+
+    return T
+
+
+def T_to_Transform(T: np.ndarray) -> Transform:
+    tf = Transform()
+
+    T_trans = T[:3, -1]
+    tf.translation = convert(T_trans, Vector3)
+
+    T_rot   = T[:3, :3]
+    q = Rotation.from_matrix(T_rot).as_quat()
+    tf.rotation = convert(q, Quaternion)
+
+    return tf
+
+
+def transform(pose: Pose, tf: Transform) -> Pose:
+    tf_OC = convert(pose, Transform)
+    T_OC  = Transform_to_T(tf_OC)
+    tf_CB = tf
+    T_CB  = Transform_to_T(tf_CB)
+
+    T_OB  = T_OC.dot(T_CB)
+    tf_OB = T_to_Transform(T_OB)
+    return convert(tf_OB, Pose)
+
+
+if __name__ == '__main__':
+    tf = Transform()
+    tf.translation.y = 8.
+    tf.rotation.x = .5
+    Transform_to_T(tf)
---
+++ robot_localization/odom_transform_node.py
@@ -0,0 +1,72 @@
+#! /usr/bin/env python3
+
+from robot_localization.dh_transform import transform
+
+# ros
+import rclpy
+from rclpy.node import Node
+from tf2_ros import Buffer, TransformException, TransformListener
+
+# msgs
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import PoseWithCovarianceStamped, TransformStamped
+
+
+class OdomTransform(Node):
+
+    def __init__(self):
+        super().__init__('odom_transform')
+        self.counter = 0
+        # get parameters
+        self.declare_parameter('odom_topic',   '/t265_camera/odom')
+        self.declare_parameter('pose_topic',   '/t265_camera/odom/to_base')
+        self.declare_parameter('target_frame', 'base_link')
+
+        self.odom_topic = self.get_parameter('odom_topic').value
+        self.pose_topic = self.get_parameter('pose_topic').value
+        self.target_frame = self.get_parameter('target_frame').value
+
+        self.get_logger().info(f'Projecting {self.odom_topic} to frame {self.target_frame} on topic {self.pose_topic}')
+
+        # initialize tf2
+        self.buffer = Buffer()
+        self.tf_listener = TransformListener(self.buffer, self)
+
+        # initialize ros communication
+        self.odom_sub = self.create_subscription(Odometry, self.odom_topic, self.odom_callback, 1)
+        self.pose_pub = self.create_publisher(PoseWithCovarianceStamped, self.pose_topic, 1)
+
+
+    def odom_callback(self, odom: Odometry) -> type(None):
+        if self.counter < 10:
+            self.counter += 1
+            return
+        else:
+            self.counter = 0
+        # create PoseWithCovarianceStamped from Odometry
+        pose                 = PoseWithCovarianceStamped()
+        pose.header          = odom.header
+        pose.pose            = odom.pose
+        pose.pose.covariance = odom.pose.covariance
+
+        # get transform from target_frame to original one
+        tf = TransformStamped()
+        try:
+            tf  = self.buffer.lookup_transform(odom.child_frame_id, self.target_frame, rclpy.time.Time())
+        except TransformException:
+            self.get_logger().warn(f'Could not get transform from {odom.child_frame_id} to {self.target_frame}')
+            return
+
+        pose.pose.pose = transform(pose.pose.pose, tf.transform)
+        self.pose_pub.publish(pose)
+
+
+def main(args=None) -> type(None):
+    rclpy.init(args=args)
+    odomTransform = OdomTransform()
+    rclpy.spin(odomTransform)
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
